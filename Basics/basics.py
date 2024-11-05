7406f4ad5af6d4de6d3164b8615aa1baf7e1a245
3DSki <atobianski@gmail.com>
2024-11-05 11:42:36 -0600

    Added more info about "import", strings, tuple.

--- Basics/basics.py
+++ Basics/basics.py
@@ -2,7 +2,14 @@
 This is an example of a multi-line comment
 
 '''
+# The import statement is used to import useful Python modules.
 import statistics
+import datetime
+# Other variations of the import statement are
+# 1) import median from statistics   - import a single object from the statistics module
+# 2) import statistics as stats      - abbreviate the module name reference for later use (i.e., stats.median())
+# 3) from statistics import median   - you don't have to use the module prefix to call median()
+# 4) from math import *              - access all objects from math with needing the math prefix
 
 # Names are in snake-case. Each part is meaningful and separated with an underscore.
 cat_count = 3
@@ -103,6 +110,10 @@ print(grades) # [100, 87, 79, 75, 62, 55]
 # Notice slice syntax!!!
 print(grades[1:3]) #  slice is [87, 79]
 
+# If it makes sense, a collection can hold different data times.
+# It is uncommon for a programming language to let you store different data types in the same list.
+my_list = [1, 2, 3, 'apple', 'banana']
+
 # Use statistics.mean() to find the average grade from grades
 print(statistics.mean(grades))
 
@@ -127,3 +138,40 @@ print(diff)
 # Shorthand for difference: -
 diff = grades_1 - grades_2  # {75, 100, 79, 87}
 print(diff)
+
+# Working with Strings
+print("Tony".upper())   # TONY
+print("TONY".lower())   # tony
+
+my_name = 'Tony'
+print(my_name.upper())          # TONY
+print(my_name.lower())          # tony
+print(my_name.find('n'))        # 2   NOTice that index positions are 0-based
+print(my_name.replace('T','M')) # MONY
+print(my_name[-1])              # y   A negative index will index from the end of a string, instead of the beginning
+print(my_name[-2:])             # ny  You can slice relative to the end of a string
+
+# The datetime module can be useful.
+today = datetime.date.today()  				    # 2024-10-27 (current date)
+print(f"Hello, {my_name}! Today is {today}.")  	# Hello, Bob! Today is 2024-10-27.
+
+# Dictionary Collection Type
+# key-value pairs: A colon separates the key aand value, while a comma separates the pairs
+# You use the key, rather than and index, to access he values.
+my_dict = {'name': 'Alice', 'age': 30, 'city': 'New York'} 
+print( "{0} {1} {2}".format(my_dict['name'], my_dict['age'], my_dict['city'])) # Alice 30 New York
+
+# Updating a value for a key...
+my_dict['age'] = 44
+print(my_dict['age'])  # 44
+
+# Tuple: an unchangeable (immutable) collection
+# Tuples are great for protecting data that's static, that you don't want to change.
+# A tuple is surrounded by parentheses.
+my_tuple = (10,30)
+print(my_tuple)   # (10,30)
+
+# You can access any value in a tuple with it's index, but you cannot change a value.
+# 
+print(my_tuple[1]) # 30
+
